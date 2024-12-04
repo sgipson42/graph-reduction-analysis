5b9901ad895c80864655e0db90eac28ba45ba536
aansels <osterling@stetson.edu>
2024-12-03 20:10:30 -0500

    Create runwithstats.py

---
+++ runwithstats.py
@@ -0,0 +1,17 @@
+import time
+import tracemalloc
+
+# records time and space while running the given function
+# returns:
+#  time taken in milliseconds
+#  the final amount of memory allocated after the function has finished
+#  the most amount of memory allocated at any point while the function runss
+def statrun(func, *args):
+    tracemalloc.start()
+    start_time = time.time_ns()
+    func(*args)
+    end_time = time.time_ns()
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    time_taken = (end_time - start_time) / 1000000
+    return time_taken, current, peak
