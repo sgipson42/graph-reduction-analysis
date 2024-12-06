64e33329f0c5514306aac9047807b8284adac8d4
aansels <osterling@stetson.edu>
2024-12-06 11:52:29 -0500

    Update runwithstats.py
    statrun(func) now also returns any output from func

--- runwithstats.py
+++ runwithstats.py
@@ -5,13 +5,14 @@ import tracemalloc
 # returns:
 #  time taken in milliseconds
 #  the final amount of memory allocated after the function has finished
-#  the most amount of memory allocated at any point while the function runss
+#  the most amount of memory allocated at any point while the function runs
+#  anything returned by the function
 def statrun(func, *args):
     tracemalloc.start()
     start_time = time.time_ns()
-    func(*args)
+    funcreturn = func(*args)
     end_time = time.time_ns()
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
     time_taken = (end_time - start_time) / 1000000
-    return time_taken, current, peak
+    return time_taken, current, peak, funcreturn
